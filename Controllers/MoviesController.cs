04faf4397bbcd99821ce2c27b86d685e1e1616be
ayaalshouha <ayaalshouha12@gmail.com>
2025-01-19 13:03:33 +0300

    add first controller

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            return View();
+        }
+    }
+}
