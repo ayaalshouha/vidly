4f82dcd3a74b18dd0c1a864fbd9fc8a727d93088
ayaalshouha <ayaalshouha12@gmail.com>
2025-01-20 14:59:53 +0300

    add first view model

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -13,6 +13,20 @@ namespace vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
+            //convention - based routing
+            //routes.MapRoute(
+            //    name: "MoviesByReleaseByDate",
+            //    url: "movies/released/{year}/{month}",
+            //    defaults: new {
+            //        Controller = "Movies",
+            //        Action = "ByReleaseDate"
+            //    },
+            //    //parameter constraints
+            //    new {year = @"2015|2016", month=@"\d{2}"});
+
+            //default routing
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -10,10 +10,33 @@ namespace vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1; 
+
+            if(string.IsNullOrWhiteSpace(sortBy))
+                sortBy  = "Name";
+
+            return Content(string.Format("pageInde={0}&sortBy={1}", pageIndex, sortBy)); 
+        }
         public ActionResult Random()
         {
             var movie = new Movie(){ Name = "Shrek"};
-            return View();
+            return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            //edit process
+
+            return Content("movie number " + id + " has been edited");
+        }
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, byte month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
