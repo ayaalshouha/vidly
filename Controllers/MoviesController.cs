5bcc21f12af653c417c407d83ab2dda6b4abda05
ayaalshouha <ayaalshouha12@gmail.com>
2025-01-20 18:08:26 +0300

    add partial view

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using vidly.Models;
+using vidly.ViewModels;
 
 namespace vidly.Controllers
 {
@@ -13,17 +14,29 @@ namespace vidly.Controllers
         public ActionResult Index(int? pageIndex, string sortBy)
         {
             if (!pageIndex.HasValue)
-                pageIndex = 1; 
+                pageIndex = 1;
 
-            if(string.IsNullOrWhiteSpace(sortBy))
-                sortBy  = "Name";
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
 
-            return Content(string.Format("pageInde={0}&sortBy={1}", pageIndex, sortBy)); 
+            return Content(string.Format("pageInde={0}&sortBy={1}", pageIndex, sortBy));
         }
         public ActionResult Random()
         {
             var movie = new Movie(){ Name = "Shrek"};
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                new Customer {Name ="Mohammad", Id=100},
+                new Customer {Name ="Ahmad", Id=101}
+            };
+
+            var viewModel = new RandomViewModelMovie
+            {
+                Movie = movie,
+                Customers = customers,
+            };
+
+            return View(viewModel);
         }
 
         public ActionResult Edit(int id)
