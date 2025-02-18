d397b38955c798ed56d5fe12d9c78c2bfaaf0b49
ayaalshouha <ayaalshouha12@gmail.com>
2025-02-18 18:11:55 +0300

    adding mapping profile and config it in global class

---
+++ App_Start/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using vidly.DTOs;
+using vidly.Models;
+
+namespace vidly.App_Start
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile() {
+            Mapper.CreateMap<Customer, CustomerDTO>();
+            Mapper.CreateMap<CustomerDTO, Customer>();
+        }
+    }
+}
