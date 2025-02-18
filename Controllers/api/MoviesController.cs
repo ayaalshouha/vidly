db36759a507d1ed413e874ad2cf0941279d4ed3e
ayaalshouha <ayaalshouha12@gmail.com>
2025-02-18 21:04:58 +0300

    add movie endpoints

--- Controllers/api/MoviesController.cs
+++ Controllers/api/MoviesController.cs
@@ -20,7 +20,34 @@ namespace vidly.Controllers.api
         [HttpGet]
         public IHttpActionResult Movies()
         {
-            return Ok(_context.Movies.ToList().Select(Mapper.Map(Movie, MovieDTO)));
+            return Ok(_context.Movies.ToList().Select(Mapper.Map<Movie, MovieDTO>));
+        }
+
+        [HttpGet]
+        public IHttpActionResult Movie(int id) {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movie = _context.Movies.FirstOrDefault(x => x.Id == id);
+            if (movie == null)
+                return NotFound();
+
+            return Ok(Mapper.Map<Movie, MovieDTO>(movie));
+        }
+
+        [HttpPost]
+        public IHttpActionResult Create(MovieDTO movieDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movie = Mapper.Map<MovieDTO, Movie>(movieDto);
+
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+            movieDto.Id = movie.Id;
+            return Created(new Uri(Request.RequestUri + "/" + movie.Id), movieDto);
         }
 
     }
