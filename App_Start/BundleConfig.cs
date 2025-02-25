1bd5150ccdf9f60afe9fbbfa02982f178163bb2b
ayaalshouha <ayaalshouha12@gmail.com>
2025-02-25 17:46:11 +0300

    update

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,24 +8,35 @@ namespace vidly
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            BundleTable.EnableOptimizations = false;
+
+
+            //bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+            //         "~/Scripts/jquery-{version}.min.js",
+            //          "~/Scripts/bootstrap.js",
+            //            "~/Scripts/respond.js",
+            //            "~/Scripts/bootbox.js",
+            //            "~/Scripts/DataTables/jquery.datatables.js", 
+            //            "~/Scripts/DataTables/datatables.bootstrap.js"
+            //     ));
+
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*"
+                        ));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-*"
+                        ));
 
-            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                       "~/Scripts/bootbox.js",
-                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/Content/bootstrap-lumen.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css"
+                      ));
         }
     }
 }
